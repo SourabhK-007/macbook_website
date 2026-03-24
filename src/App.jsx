a937f31f0a2c92c2d903f727d36a106daf199d4d
SourabhK-007 <sourabhkarikatti007@gmail.com>
2026-03-24 11:21:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {useRef} from 'react'
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+
+const App = () => {
+
+
+    return (
+        <main >
+            <Navbar/>
+            <Hero/>
+        </main>
+    )
+}
+export default App
